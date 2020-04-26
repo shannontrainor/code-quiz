fac7b844f7291ec2cb7af8f19d97605c6585a9cd
Shannon Trainor <sltrainor@outlook.com>
2020-04-26 13:52:10 -0400

    added message display

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //upon loading game user will see all instructions (part of HTML)
 //instructions in div id of #start
 
+//variables for DOM elements
+var currentQuestionIndex = 0;
+var questionsElement = document.getElementById("questions");
+var optionsElement = document.getElementById("options");
+var timerElement = document.getElementById("time");
+var messageElement = document.getElementById("message")
 
-var questionsElement = document.getElementById("question");
 //define time for quiz
 var quizTime = questions.length * 15;
 var quizTimer;
-var optionsElement = document.getElementById("options");
 
 //*****************START GAME***************************
 // WHEN I click the start button
@@ -22,17 +26,68 @@ function startQuiz(){
    //start timer
     quizTimer = setInterval(timeCount, 1000)
     //show timer on page
-    quizTimer.textContent = time;
+    timerElement.textContent = quizTime;
+
+    generateQuestion();
+}
+
+//generate question with multiple choice answers
+function generateQuestion() {
+    //get current question from questions.js array
+    var currentQuestion = questions[currentQuestionIndex];
+    //will get first question
+
+        //will change question title to current
+    var titleElement = document.getElementById("question-title");
+    titleElement.textContent = currentQuestion.title;
+   
+    //remove past questions
+    optionsElement.innerHTML = "";
+
+    currentQuestion.options.forEach(function(option, i){
+        //new button created for each multiple choice option
+        var optionButton = document.createElement("button");
+        optionButton.setAttribute("class", "option");
+        optionButton.setAttribute("value", option);
+
+        optionButton.textContent = i + 1 + ". " + option;
+        //event listener for question options
+        optionButton.onclick = questionClicked;  //will be new function
+
+        //displays on page
+        optionsElement.appendChild(optionsButton);
+    });
+
+}
+//function to subtract time if answer is incorrect
+function questionClicked(){
+    if (this.value !== questions[currentQuestionIndex].answer){
+        quizTime -= 15;
+
+        if (time < 0) {
+            time = 0;
+        }
+        //displays new time
+        timerElement.textContent = quizTime;
+
+        messageElement.textContent = "Incorrect";       
+    } else {
+        messageElement.textContent = "Correct";
+    }
+        messageElement.setAttribute("class", "message");
+        setTimeout(function(){
+            messageElement.setAttribute("class", "message hide");
+        }, 1000);
+
+
+
+
 }
 
 function timeCount (){
     // subtract time
-    time--;
-    quizTimer.textContent = time;
-}
-    // generate first question with choice buttons
-function generateQuestion() {
-
+    quizTime--;
+    timerElement.textContent = time;
 }
 
     //store time in variable quizTime, default at 60 s.
