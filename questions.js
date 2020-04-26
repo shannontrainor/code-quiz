8a4ec695e54bb083d6a9a2cbdd00f8368560ff37
Shannon Trainor <sltrainor@outlook.com>
2020-04-26 17:07:14 -0400

    edited questions

--- questions.js
+++ questions.js
@@ -1,67 +1,39 @@
 //array of questions, options, answers
 
 var questions = [
-{
-    title: "What does CSS stand for?"
-    options: ["Color Coding Styles", "Cascading Style Sheets", "Collection of Selective Styles", "Clean Code Styles" ]
-    answer: "Cascading Style Sheets"
-},
+    {
+        title: "What does CSS stand for?",
+        options: ["Color Coding Styles", "Cascading Style Sheets", "Collection of Selective Styles", "Clean Code Styles" ],
+        answer: "Cascading Style Sheets"
+    },
 
-{
-    title: "Which of the following represents snake casing?"
-    options: ["thisIsSnakeCasing", "this-is-snake-casing", "this_is_snake_casing", "ThisIsSnakeCasing"]
-    answer: "this_is_snake_casing"
-},
+    {
+        title: "Which of the following represents snake casing?",
+        options: ["thisIsSnakeCasing", "this-is-snake-casing", "this_is_snake_casing", "ThisIsSnakeCasing"],
+        answer: "this_is_snake_casing",
+    },
 
-{
-    title: "What is the main advantage of using console.log?"
-    options: ["To hide your code", "To display your code", "To debug your code", "To bug your code"]
-    answer: "To debug your code"
-},
+    {
+        title: "What is the main advantage of using console.log?",
+        options: ["To hide your code", "To display your code", "To debug your code", "To bug your code"],
+        answer: "To debug your code"
+    },
 
-{
-    title: "What CSS framework do we most commonly use to develop responsive websites?"
-    options: ["Shoelace", "Sandlestrap", "Velcro", "Bootstrap"]
-    answer: ["Bootstrap"]
-},
+    {
+        title: "What CSS framework do we most commonly use to develop responsive websites?",
+        options: ["Shoelace", "Sandlestrap", "Velcro", "Bootstrap"],
+        answer: ["Bootstrap"]
+    },
 
-{
-    title: "What does JSON stand for?"
-    options: ["Jason Derulo", "Javascript Object Notation", "Javascript Object Notorization ", "Jalapeno Sourcream and Olive Nachos "]
-    answer: "Javacsript Object Notation"
-},
+    {
+        title: "What does JSON stand for?",
+        options: ["Jason Derulo", "Javascript Object Notation", "Javascript Object Notorization ", "Jalapeno Sourcream and Olive Nachos "],
+        answer: "Javascript Object Notation"
+    },
 
-{
-    title: "How difficult did I find this homework assignment?"
-    options: ["Extremely difficult", "Only a little difficult", "Not difficult", "What homework assignment?"]
-    answer: "Extremely difficult"
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-]
+    {
+        title: "How difficult did I find this homework assignment?",
+        options: ["Extremely difficult", "Only a little difficult", "Not difficult", "What homework assignment?"],
+        answer: "Extremely difficult"
+    }
+];
